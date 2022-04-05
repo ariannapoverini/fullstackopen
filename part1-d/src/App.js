d3f3b78304f6b9fa51dffaad666f8e4584f8ce9a
ariannapoverini <arianna.poverini@gmail.com>
2022-04-05 17:03:05 +0200

    exercise 1.6 unicafe step1

--- part1-d/src/App.js
+++ part1-d/src/App.js
@@ -1,12 +1,31 @@
 import { useState } from "react";
+import { Display } from "./components/Display";
+import { Button } from "./components/Button";
+import { Title } from "./components/Title";
 
 const App = () => {
+  const title = {
+    titles: ["give feedback", "statistics"],
+  };
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <Title text={title.titles[0]} />
+      <Button onClick={handleGood} text={"Good"} />
+      <Button onClick={handleNeutral} text={"Neutral"} />
+      <Button onClick={handleBad} text={"Bad"} />
+      <Title text={title.titles[1]} />
+      <Display good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
 };
 
 export default App;
---
+++ part1-d/src/components/Display.js
@@ -0,0 +1,9 @@
+export const Display = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <p> Good: {good} </p>
+      <p> Neutral: {neutral} </p>
+      <p> Bad: {bad} </p>
+    </div>
+  );
+};
