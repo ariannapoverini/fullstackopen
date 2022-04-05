9d2eee44641714a8d12210a71a1de7b77d0d4c9a
ariannapoverini <arianna.poverini@gmail.com>
2022-04-05 18:44:43 +0200

    exercise 1.7 unicafe step2

--- part1-d/src/App.js
+++ part1-d/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Display } from "./components/Display";
 import { Button } from "./components/Button";
 import { Title } from "./components/Title";
+import { Display } from "./components/Display";
 
 const App = () => {
   const title = {
@@ -11,6 +11,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + bad;
 
   const handleGood = () => setGood(good + 1);
   const handleNeutral = () => setNeutral(neutral + 1);
@@ -23,7 +24,7 @@ const App = () => {
       <Button onClick={handleNeutral} text={"Neutral"} />
       <Button onClick={handleBad} text={"Bad"} />
       <Title text={title.titles[1]} />
-      <Display good={good} neutral={neutral} bad={bad} />
+      <Display good={good} neutral={neutral} bad={bad} total={total} />
     </div>
   );
 };
--- part1-d/src/components/Display.js
+++ part1-d/src/components/Display.js
@@ -1,9 +1,12 @@
-export const Display = ({ good, neutral, bad }) => {
+export const Display = ({ good, neutral, bad, total }) => {
   return (
     <div>
       <p> Good: {good} </p>
       <p> Neutral: {neutral} </p>
       <p> Bad: {bad} </p>
+      <p> Total: {total} </p>
+      <p> Average: {good * 1 - (bad * 1) / (good + neutral + bad)}</p>
+      <p> Positive: {(good / total) * 100}</p>
     </div>
   );
 };
