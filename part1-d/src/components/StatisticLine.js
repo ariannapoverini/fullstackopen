6f8ba006e457cf553176dcefcc7da1b6d64f5970
ariannapoverini <arianna.poverini@gmail.com>
2022-04-05 19:38:33 +0200

    exercise 1.11 unicafe step6

--- part1-d/src/components/StatisticLine.js
+++ part1-d/src/components/StatisticLine.js
@@ -1,10 +1,10 @@
 export const StatisticLine = ({ text, value }) => {
   return (
     <>
-      <p>
-        {" "}
-        {text}: {value}
-      </p>
+      <tr>
+        <td>{text}:</td>
+        <td>{value}</td>
+      </tr>
     </>
   );
 };
--- part1-d/src/components/Statistics.js
+++ part1-d/src/components/Statistics.js
@@ -4,15 +4,20 @@ export const Statistics = ({ good, neutral, bad, total }) => {
   if (total !== 0) {
     return (
       <div>
-        <StatisticLine text={"Good"} value={good} />
-        <StatisticLine text={"Neutral"} value={neutral} />
-        <StatisticLine text={"Bad"} value={bad} />
-        <StatisticLine text={"Total"} value={total} />
-        <StatisticLine
-          text={"Average"}
-          value={good * 1 - (bad * 1) / (good + neutral + bad)}
-        />
-        <StatisticLine text={"Positive"} value={(good / total) * 100} />
+        <table>
+          <thead></thead>
+          <tbody>
+            <StatisticLine text={"Good"} value={good} />
+            <StatisticLine text={"Neutral"} value={neutral} />
+            <StatisticLine text={"Bad"} value={bad} />
+            <StatisticLine text={"Total"} value={total} />
+            <StatisticLine
+              text={"Average"}
+              value={good * 1 - (bad * 1) / (good + neutral + bad)}
+            />
+            <StatisticLine text={"Positive"} value={(good / total) * 100} />
+          </tbody>
+        </table>
       </div>
     );
   }
