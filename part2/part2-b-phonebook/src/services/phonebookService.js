86073dd37e1321d67deab92657d139cf33cce48e
Arianna Poverini <arianna.poverini@gmail.com>
2022-05-16 10:31:12 +0200

    phonebookUrl change

--- part2/part2-b-phonebook/src/services/phonebookService.js
+++ part2/part2-b-phonebook/src/services/phonebookService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const phonebookUrl = "http://localhost:3001/api/persons";
+const phonebookUrl = "/api/persons";
 
 const getPersons = () => {
   return axios.get(phonebookUrl);
